fc63e5736b95e1128759eda62c54ec8ad5debca7
John Kleinad <juan_mejia13@outlook.com>
2024-02-14 11:01:22 -0600

    add validation

--- src/components/InfoBlock.jsx
+++ src/components/InfoBlock.jsx
@@ -1,9 +1,14 @@
 const InfoBlock = ({ icon, data, type }) => {
 
+    const types = {
+        tel: 'tel',
+        mail:'mailto',
+        adress:'adress'
+    }
     return <>
         <div className="flex items-center gap-2 justify-center text-white text">
             {icon}
-            <a href="tel:3333541774">{data}</a>
+            <a href={`${types[type]}:${data}`}>{data}</a>
         </div>
     </>
 }
